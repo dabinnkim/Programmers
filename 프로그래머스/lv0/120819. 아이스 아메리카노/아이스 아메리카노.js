6d2c6ccddc5184d2c1e106e94c2421f937ad25f9
dabinnkim <107906438+dabinnkim@users.noreply.github.com>
2023-03-26 00:00:11 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -1,6 +1,6 @@
 function solution(money) {
     var answer = [];
-    answer[0]=Math.floor(money/5500);
+    answer[0]=Math.floor(money/5500); //Math.floor : 소수점 버림
     answer[1]=money%5500;
     return answer;
 }
