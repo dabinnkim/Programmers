3551edba09c5af74e52405427b6cd0662e36ad88
dabinnkim <107906438+dabinnkim@users.noreply.github.com>
2023-04-04 01:15:45 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
@@ -1,10 +1,21 @@
-function solution(n, k) {
+// function solution(n, k) {
 
-    if(n>=10){
-         var 서비스 = Math.floor(n/10)
-         return (n*12000) + ((k-서비스)*2000)
-    }else{
-        return n*12000 + k*2000
-    }
+//     if(n>=10){
+//          var 서비스 = Math.floor(n/10)
+//          return (n*12000) + ((k-서비스)*2000)
+//     }else{
+//         return n*12000 + k*2000
+//     }
     
+// }
+
+
+function solution(n, k) {
+  const costOfFood = n * 12000;  // 양꼬치 가격 계산
+  const numOfFreeDrinks = Math.floor(n / 10);  // 무료 음료 개수 계산
+  const numOfPaidDrinks = k - numOfFreeDrinks;  // 유료 음료 개수 계산
+  const costOfDrinks = numOfPaidDrinks * 2000;  // 유료 음료 가격 계산
+  const totalCost = costOfFood + costOfDrinks;  // 총 지불 금액 계산
+  
+  return totalCost;
 }
