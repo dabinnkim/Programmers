cbbfbb8da54c57cd6a313bee17f39a3e9838c26e
dabinnkim <107906438+dabinnkim@users.noreply.github.com>
2023-04-04 00:01:48 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.js
+++ 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.js
@@ -1,7 +1,11 @@
-function solution(strlist) {
-    var answer = []
-    for(let i = 0; i<strlist.length; i++){
-        answer.push(strlist[i].length)
-    }
-    return answer
+// function solution(strlist) {
+//     var answer = []
+//     for(let i = 0; i<strlist.length; i++){
+//         answer.push(strlist[i].length)
+//     }
+//     return answer
+// }
+
+function solution(strlist){
+    return strlist.map((e)=>e.length)
 }
