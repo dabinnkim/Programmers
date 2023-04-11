81c1a07e1b593ddad0a2e3095ff34e2b406ac9b0
dabinnkim <107906438+dabinnkim@users.noreply.github.com>
2023-04-11 17:44:11 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.02 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,15 +1,11 @@
-function solution(arr) {
-  let answer = [];
-  let sortedArr = [...arr].sort((a, b) => b - a);
+function solution(arr){
+    let answer = []; //빈배열 만들기
+    let sortedArr = [...arr].sort((a, b) => b-a); //내림차순
 
-  for (let i = 0; i < sortedArr.length; i++) {
-    answer.push(sortedArr.indexOf(arr[i]) + 1);
-  }
-
-  return answer;
+    for(let i =0; i<arr.length; i++){
+        answer.push(sortedArr.indexOf(arr[i])+1) //sortedArr의 배열에서 arr[i]와 같은 요소를 찾고 찾은 요소의 인덱스에 1을 더한 값을 answer에 push
+    }
+    return answer
 }
-let arr = [87, 89, 92, 100, 76];
 
-/*
-내림차순으로 정렬 후 1,2,3 순위 결정
-*/
+console.log(solution([29,100,38,2,3]))
