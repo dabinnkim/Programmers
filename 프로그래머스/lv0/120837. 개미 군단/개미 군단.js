120d5c5acc42fac252f8770d22a75418e6db5d33
dabinnkim <107906438+dabinnkim@users.noreply.github.com>
2023-04-14 00:13:07 +0900

    [level 0] Title: 개미 군단, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -1,6 +1,10 @@
 function solution(hp) {
-  const first = Math.floor(hp / 5);
-  const second = Math.floor((hp - first * 5) / 3);
-  const third = hp - first * 5 - second * 3;
-  return first + second + third;
+    const 장군 = Math.floor(hp/5); //hp를 5로 나눈 정수 몫 구하기
+    const 병정 = Math.floor(hp%5/3); //hp를 5로 나누고 그 나머지 값을 3으로 나누기
+    const 일 = hp - (장군*5+병정*3)
+  
+  return 장군 + 병정 + 일
 }
+
+//1. 5로 나누기
+//2. 3으로 나누기
