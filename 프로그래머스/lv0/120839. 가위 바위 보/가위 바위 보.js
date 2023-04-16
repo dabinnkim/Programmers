7d7c5390f0eee54e2db1741201a5ca534f663520
dabinnkim <107906438+dabinnkim@users.noreply.github.com>
2023-04-16 17:11:50 +0900

    [level 0] Title: 가위 바위 보, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,13 +1,32 @@
+//① 객체만들기
 function solution(num){
-    const arr =[]
-    num.split('').map(e=>{
-        if (e==0){
-            arr.push(5)
-        }else if(e==2){
-            arr.push(0)
-        }else{
-            arr.push(2)
-        }
-    })
-    return arr.join('')
+   let arr = {
+    0:5,
+    2:0,
+    5:2
+   }
+   return [...num].map(e=>arr[e]).join('')
 }
+
+//② map()안에서 삼항연산자
+/*
+function solution1(num1){
+    return [...num1].map(e=>
+        e==0? 5:(e==2? 0:2)
+    ).join('')
+}
+*/
+
+// function solution(num){
+//     const arr =[]
+//     num.split('').map(e=>{
+//         if (e==0){
+//             arr.push(5)
+//         }else if(e==2){
+//             arr.push(0)
+//         }else{
+//             arr.push(2)
+//         }
+//     })
+//     return arr.join('')
+// }
