c6f6b053a2abd6f07d15e52135ec9cdf25e59ce2
dabinnkim <107906438+dabinnkim@users.noreply.github.com>
2023-04-19 12:10:21 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,15 +1,22 @@
-function solution(num_list, n) {
-    // 정답배열 생성
-    const result = []
-    // 전 범위 순환
-    for(let i = 0 ; i < num_list.length; ) {
-        const sliceN = []
-        // n만큼 배열 생성
-        for(let j = 0 ; j < n ; j++) {
-            sliceN.push(num_list[i])
-            i++
-        }
-        result.push(sliceN)
-    }
-    return result
+function solution(num_list,n){
+    const arr = []
+    
+    while(num_list.length){
+        arr.push(num_list.splice(0,n)) //splice : 인덱스0부터 n개의 요소 추출
+    }return arr
 }
+// function solution(num_list, n) {
+//     // 정답배열 생성
+//     const result = []
+//     // 전 범위 순환
+//     for(let i = 0 ; i < num_list.length; ) {
+//         const sliceN = []
+//         // n만큼 배열 생성
+//         for(let j = 0 ; j < n ; j++) {
+//             sliceN.push(num_list[i])
+//             i++
+//         }
+//         result.push(sliceN)
+//     }
+//     return result
+// }
