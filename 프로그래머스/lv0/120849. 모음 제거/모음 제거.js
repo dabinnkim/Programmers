4a5480980bee1e6af035a4cd86081a1521a73311
dabinnkim <107906438+dabinnkim@users.noreply.github.com>
2023-04-20 19:03:55 +0900

    Update 모음 제거.js

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
@@ -9,3 +9,7 @@ function solution(my_string){
     const arr = my_string.split('') //배열로 만들기
     return arr.filter(e => e=!answer[e]).join('')
 }
+
+//문장을 split로 배열로 만들고
+//filter 사용해 'answer 객체에 없는 문자'를 조건으로 줘서 새로운 배열로 만들고 
+//join으로 문자열로 만들기
