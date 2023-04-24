ddce06c133d699497a654a90ef579b2f8162e66f
dabinnkim <107906438+dabinnkim@users.noreply.github.com>
2023-04-24 21:18:51 +0900

    [unrated] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,7 +1,10 @@
-function solution(a, b) {
-    const aa = a.toString()+b.toString()
-    const bb = b.toString()+a.toString()
-    if(aa>bb){
-        return Number(aa)
-    }else{return Number(bb)}
+function solution(a,b){
+    return Math.max(`${a}${b}`,`${b}${a}`)
 }
+// function solution(a, b) {
+//     const aa = a.toString()+b.toString()
+//     const bb = b.toString()+a.toString()
+//     if(aa>bb){
+//         return Number(aa)
+//     }else{return Number(bb)}
+// }
