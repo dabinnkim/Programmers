4b9258afc8377a6c20187a3cfcdf85c02acd18c0
dabinnkim <107906438+dabinnkim@users.noreply.github.com>
2023-05-13 00:58:25 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.54 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,11 +1,15 @@
 function solution(str){
-  const arr = str.split('');
-  let newArr = '';
-  for(let i =0; i<arr.length; i++){
-    if(arr[i]===arr[i].toUpperCase()){
-      newArr+=arr[i].toLowerCase()
-    }else if(arr[i]===arr[i].toLowerCase()){
-      newArr+=arr[i].toUpperCase()
-    }
-  }return newArr
+    return str.split('').map((n)=> n===n.toUpperCase()? n.toLowerCase():n.toUpperCase()).join('')
 }
+
+// function solution(str){
+//   const arr = str.split('');
+//   let newArr = '';
+//   for(let i =0; i<arr.length; i++){
+//     if(arr[i]===arr[i].toUpperCase()){
+//       newArr+=arr[i].toLowerCase()
+//     }else if(arr[i]===arr[i].toLowerCase()){
+//       newArr+=arr[i].toUpperCase()
+//     }
+//   }return newArr
+// 
