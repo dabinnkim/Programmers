11d6683f02255793101b207fc7781dd2c562fa06
dabinnkim <107906438+dabinnkim@users.noreply.github.com>
2023-07-19 16:02:06 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.02 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -1,18 +1,22 @@
-function solution(dot) {
-  //둘다 양수
-  if (dot[0] > 0 && dot[1] > 0) {
-    return 1;
-  }
-  //0은 양수, 1은 음수
-  else if (dot[0] > 0 && dot[1] < 0) {
-    return 4;
-  }
-  //0은 음수, 1은 양수
-  else if (dot[0] < 0 && dot[1] > 0) {
-    return 2;
-  }
-  //둘다 음수
-  else {
-    return 3;
-  }
+function solution(dot){
+    return dot[0]>0? (dot[1]>0?1:4):(dot[1]>0?2:3)
 }
+
+// function solution(dot) {
+//   //둘다 양수
+//   if (dot[0] > 0 && dot[1] > 0) {
+//     return 1;
+//   }
+//   //0은 양수, 1은 음수
+//   else if (dot[0] > 0 && dot[1] < 0) {
+//     return 4;
+//   }
+//   //0은 음수, 1은 양수
+//   else if (dot[0] < 0 && dot[1] > 0) {
+//     return 2;
+//   }
+//   //둘다 음수
+//   else {
+//     return 3;
+//   }
+// }
